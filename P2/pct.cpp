75ece87b46d3c1efc208d5ff55b0603a959d906a
baikman <baikman@cedarville.edu>
2025-09-24 21:43:11 -0400

    i just be consuming idk

--- P2/pct.cpp
+++ P2/pct.cpp
@@ -12,24 +12,42 @@ mutex the_mutex;
 condition_variable_any condc, condp;
 int buffer = 0;
 
-void *producer(void *ptr) {
+void producer(void *ptr) {
     int i;
-    the_mutex.lock();
-    while (buffer != 0) {
-        the_mutex.unlock();
-        condp.wait(the_mutex);
+    for (i = 1; i <= MAX; i++) {
         the_mutex.lock();
+        while (buffer != 0) {
+            the_mutex.unlock();
+            condp.wait(the_mutex);
+            the_mutex.lock();
+        }
+        buffer = i;
+        condc.notify_one();
+        the_mutex.unlock();
     }
-    buffer = i;
-    condc.notify_one();
-    the_mutex.unlock();
+    return;
 }
 
-void *consumer(void *ptr) {
+void consumer(void *ptr) {
     int i;
+    for (i = 1; i <= MAX; i++) {
+        the_mutex.lock();
+        while (buffer == 0) {
+            the_mutex.unlock();
+            condc.wait(the_mutex);
+            the_mutex.lock();
+        }
+        buffer = 0;
+        condp.notify_one();
+        the_mutex.unlock();
+    }
+    return;
 }
 
 int main (int argc, char *argv[]) {
-    cout << "hi im working on it trust" << endl;
+    thread pro(producer);
+    thread con(consumer);
+    pro.join();
+    con.join();
     return 0;
 }
