4ebd0b78d72fde664691f1e2f61d2ed4db093a6e
baikman <baikman@cedarville.edu>
2025-09-24 22:50:44 -0400

    added docs lol

--- P2/pct.cpp
+++ P2/pct.cpp
@@ -1,3 +1,15 @@
+/*********************************************************
+* Summary: This file includes a solution to the producer-
+*           consumer problem using threads.
+*
+* Author: Brandon Aikman
+* Created: Sep 2025
+* Summary of Modifications:
+* 24 Sep 2025 – Initial version.
+*
+* ©Copyright Cedarville University, its Computer Science faculty, and the
+* authors. All rights reserved.
+********************************************************/
 #include <stdio.h>
 #include <iostream>
 #include <thread>
@@ -5,51 +17,64 @@
 #include <condition_variable>
 #include <chrono>
 
-#define MAX 1000000000
+#define MAX 12
 
 using namespace std;
 
 mutex the_mutex;
-condition_variable_any condc, condp;
-int buffer = 0;
+condition_variable_any condc, condp;    // Condition variables for consumer and producer threads
+const int N = 7;                        // Size of the buffer
+char buff[N];
+int in = 0;                             // producer counter
+int out = 0;                            // consumer counter
+int count = 0;                          // counter
 
-void producer(void *ptr) {
-    int i;
-    for (i = 1; i <= MAX; i++) {
-        the_mutex.lock();
-        while (buffer != 0) {
-            the_mutex.unlock();
-            condp.wait(the_mutex);
-            the_mutex.lock();
+void producer() {
+    for (int i = 0; i < MAX; i++) {
+        unique_lock<mutex> lock(the_mutex); // Lock the mutex
+
+        while (count == N) {
+            condp.wait(lock); // When at the final index, lock this thread
         }
-        buffer = i;
-        condc.notify_one();
-        the_mutex.unlock();
+
+        char c = 'A' + rand() % 26; // Set c to a random capital letter
+        buff[in] = c; // Place c in the buffer
+        in = (in + 1) % N; // Circular increment
+        count++;
+
+        cout << (char)tolower(c) << endl;
+
+        condc.notify_one(); // Notify consumer thread
     }
     return;
 }
 
-void consumer(void *ptr) {
-    int i;
-    for (i = 1; i <= MAX; i++) {
-        the_mutex.lock();
-        while (buffer == 0) {
-            the_mutex.unlock();
-            condc.wait(the_mutex);
-            the_mutex.lock();
+void consumer() {
+    for (int i = 0; i < MAX; i++) {
+        unique_lock<mutex> lock(the_mutex); // Lock the mutex
+
+        while (count == 0) {
+            condc.wait(lock); // When at the first (zero-indexed) index, lock this thread
         }
-        buffer = 0;
-        condp.notify_one();
-        the_mutex.unlock();
+
+        char c = buff[out]; // Grab c from the buffer
+        out = (out + 1) % N; // Circular increment
+        count--;
+
+        cout << c << endl;
+
+        condp.notify_one(); // Notify producer thread
     }
     return;
 }
 
 int main (int argc, char *argv[]) {
-    thread pro(producer);
-    thread con(consumer);
+    cout << "The Producer-Consumer Problem\nBuffer Size: " << N << "\n[p]roducer / [C]onsumer - " << MAX << " items" << endl;
+    thread pro(producer); // Create producer thread
+    this_thread::sleep_for(chrono::seconds(1)); // Sleep to allow producer to start
+    thread con(consumer); // Create consumer thread
     pro.join();
-    this_thread::sleep_for(chrono::seconds(1));
     con.join();
+    cout << "Inter-process communication complete." << endl;
     return 0;
 }
